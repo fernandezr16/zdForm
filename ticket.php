087b9af1eb7288fc2131dc8433cc75fbb4b45d29
fernandezr16 <fernandezr16@gmail.com>
2016-03-15 14:56:04 -0700

    Update ticket.php

--- ticket.php
+++ ticket.php
@@ -20,7 +20,7 @@ function curlWrap($url, $json, $action)
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-  curl_setopt($curl_ch, CURLOPT_CAINFO, dirname(__FILE__)."cacert.pem");
+  	curl_setopt($curl_ch, CURLOPT_CAINFO, dirname(__FILE__)."cacert.pem");
 	$output = curl_exec($ch);
 	curl_close($ch);
 	$decoded = json_decode($output);
@@ -31,7 +31,7 @@ foreach($_POST as $key => $value){
 		$arr[strip_tags($key)] = strip_tags($value);
 	}
 }
-$create = json_encode(array('ticket' => array('subject' => $arr['z_subject'], 'comment' => array('value' => $arr['z_description']), 'requester' => array('name' => $arr['z_name'], 'email' => $arr['z_requester']))));
+$create = json_encode(array('ticket' => array('subject' => $arr['z_subject'], 'comment' => array('pub' => $arr['z_pub']), 'comment' => array('value' => $arr['z_description']), 'requester' => array('name' => $arr['z_name'], 'email' => $arr['z_requester']))));
 $return = curlWrap("/tickets.json", $create);
 
 // Redirect to success page else 404 page.
