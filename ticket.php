3433cee819e52054a2776034a305189d31cf75f2
fernandezr16 <fernandezr16@gmail.com>
2016-03-16 13:35:02 -0700

    Update ticket.php

--- ticket.php
+++ ticket.php
@@ -30,7 +30,7 @@ foreach($_POST as $key => $value){
 		$arr[strip_tags($key)] = strip_tags($value);
 	}
 }
-$create = json_encode(array('ticket' => array('subject' => $arr['z_subject'], 'custom_fields' => array('native_ad_unit','content_landing_page','feed_relay', 'DFP','articles_by_author','admin_pa_ui_error' => $arr['z_31204387'], 'comment' => array( "value"=> $arr['z_description']), 'requester' => array('name' => $arr['z_name'], 'email' => $arr['z_requester']))));
+$create = json_encode(array('ticket' => array('subject' => $arr['z_subject'], 'comment' => array( "value"=> $arr['z_description']), 'requester' => array('name' => $arr['z_name'], 'email' => $arr['z_requester']))));
 $return = curlWrap("/tickets.json", $create);
 
 // Redirect to success page else 404 page.
